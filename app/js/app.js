b6f54e960a7b5edf2d30929e19d71cecd5266f6f
BobSorando <v.lpwebbox@gmail.com>
2021-05-13 15:31:41 +0300

    New less starter

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,42 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	// $('[data-fancybox="gallery"]').fancybox();
+
+	// var hamburger = $(".header_menu");
+	// var body = $('body');
+	// hamburger.on('click', function(){
+	// 	hamburger.find('.hamburger').toggleClass("is-active");
+	// 	body.toggleClass('menu-open');
+	// })
+
+	// $('.slider').on('initialized.owl.carousel', function(event) {
+
+	// });
+
+	// $('.modal').on('click', function(){
+	// 	var modal_type = $(this).data('modal');
+	// 	$('.overlay').fadeIn();
+	// 	$('div.modal.'+modal_type).fadeIn();
+	// })
+	// $('.overlay, .close, .modal.success button').on('click', function(){
+	// 	$('.overlay').fadeOut();
+	// 	$('div.modal').fadeOut();
+	// })
+
+	// $('form input').on('click', function(){
+	// 	$(this).removeClass('error');
+	// })
+
+	// $('form button[type="submit"]').on('click', function(){
+	// 	var inputs = $(this).parents('form').find('input[required]');
+	// 	inputs.each(function(){
+	// 		if($(this).is(':invalid')){
+	// 			$(this).parent().addClass('invalid');
+	// 		}
+	// 	})
+	// })
+
 	// Custom JS
 
 })
